0d800ad6e95a277255d31f5ecc2f610553b30a6a
Stas <stasfuryus@gmail.com>
2024-02-10 13:15:21 +0300

    bug fix with  kb.language = 'CH'

--- src/keyboard.py
+++ src/keyboard.py
@@ -3,13 +3,19 @@ from src.item import Item
 
 class Keyboard(Item):
     """Класс для представления товара в магазине, тип клавиатура"""
+
     def __init__(self, name, price, quantity):
         super().__init__(name, price, quantity)
-        self.language = "EN"
+        self._language = "EN"
+
+    @property
+    def language(self):
+        return self._language
 
     def change_lang(self):
         """Меняет язык раскладки клавиатуры между RU и EN"""
-        if self.language == "EN":
-            self.language = "RU"
-        elif self.language == "RU":
-            self.language = "EN"
+
+        if self._language == "EN":
+            self._language = "RU"
+        elif self._language == "RU":
+            self._language = "EN"
