092ddfed1ad0cdd8c657fca3fb28e2fab799bc1e
Stas <stasfuryus@gmail.com>
2024-02-11 05:39:38 +0300

    варианты с миксином(MixinLenguage) и без

--- src/keyboard.py
+++ src/keyboard.py
@@ -1,11 +1,8 @@
 from src.item import Item
 
 
-class Keyboard(Item):
-    """Класс для представления товара в магазине, тип клавиатура"""
-
-    def __init__(self, name, price, quantity):
-        super().__init__(name, price, quantity)
+class MixinLenguage:
+    def __init__(self):
         self._language = "EN"
 
     @property
@@ -19,3 +16,24 @@ class Keyboard(Item):
             self._language = "RU"
         elif self._language == "RU":
             self._language = "EN"
+
+
+class Keyboard(Item, MixinLenguage):
+    """Класс для представления товара в магазине, тип клавиатура"""
+
+    def __init__(self, name, price, quantity):
+        super().__init__(name, price, quantity)
+        self._language = "EN"
+
+    # вариант без миксина
+    # @property
+    # def language(self):
+    #     return self._language
+
+    # def change_lang(self):
+    #     """Меняет язык раскладки клавиатуры между RU и EN"""
+    #
+    #     if self._language == "EN":
+    #         self._language = "RU"
+    #     elif self._language == "RU":
+    #         self._language = "EN"
